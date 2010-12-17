dd44fc8cbe8f8572a1c03ec8424dda30ee3a5d2d
Jacek Zlydach <temporal.pl@gmail.com>
2010-12-17 13:47:02 +0100

    Arrow keys are now banned from EMACS.

---
+++ .emacs.conf/40 keys.el
@@ -0,0 +1,15 @@
+;;;; Code taken from http://news.ycombinator.com/item?id=2007589
+  (defun  disabled-key ()
+    "Assign this to disable a key"
+    (interactive)
+    (print "All your arrow keys are belong to me. Have a nice day!"))
+
+  ;; All your arrow keys are disabled Hahahah
+  (global-set-key (kbd "<up>")      'disabled-key)
+  (global-set-key (kbd "<down>")    'disabled-key)
+  (global-set-key (kbd "<left>")    'disabled-key)
+  (global-set-key (kbd "<right>")   'disabled-key)
+  (global-set-key (kbd "<C-up>")      'disabled-key)
+  (global-set-key (kbd "<C-down>")    'disabled-key)
+  (global-set-key (kbd "<C-left>")    'disabled-key)
+  (global-set-key (kbd "<C-right>")   'disabled-key)
