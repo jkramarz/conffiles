d26a6858762e1aa9180899598db5d0105faf2ee3
TeMPOraL <temporal.pl@gmail.com>
2013-08-12 02:32:17 +0200

    fish functions

---
+++ fish/functions/fish_right_prompt.fish
@@ -0,0 +1,37 @@
+function fish_right_prompt -d "Write out the right prompt"
+
+    set retcode $status
+    if test $retcode -eq 0
+       set_color normal
+    else
+       set_color red
+    end
+    echo -n "$retcode "
+    set_color normal
+
+    set temp1 (sensors -u | grep temp3_input | awk '{print $2}')
+    set temp2 (sensors -u | grep temp3_input | awk '{print $2}')
+
+    if test (echo "$temp1 < 70" | bc) -eq 1
+       set_color green
+    else
+       set_color red
+    end
+    printf "%.0f" $temp1
+
+    set_color 555
+    echo "/"
+
+    if test (echo "$temp1 < 70" | bc) -eq 1
+       set_color green
+    else
+       set_color red
+    end
+    printf "%.0f" $temp2
+
+    set_color 555
+    echo "℃  "
+    date "+%H:%m:%S %d-%m-%Y"
+
+    set_color normal
+end
